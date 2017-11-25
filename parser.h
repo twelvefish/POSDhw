6478fba4515e0029155ddf16d2460f070135e287
Kevin <amingo08131@gmail.com>
2017-11-25 17:32:55 +0800

    test error 5、11

--- parser.h
+++ parser.h
@@ -9,7 +9,7 @@ using std::string;
 #include "scanner.h"
 #include "struct.h"
 #include "list.h"
-
+#include "node.h"
 #include "utParser.h"
 
 class Parser{
@@ -72,6 +72,69 @@ public:
     return _terms;
   }
 
+  void matchings(){
+    Term* term = createTerm();
+    if(term!=nullptr)
+    { 
+      if(isCOMMA==1){
+        Term * findTerm = find(term);
+        if(findTerm != nullptr) term->match(*findTerm);
+      }
+      _terms.push_back(term);
+      while((_currentToken = _scanner.nextToken()) == ',' ||  _currentToken=='='|| _currentToken == ';') {
+        if (_currentToken == '=') {
+          isCOMMA = 0;
+          Node * left = new Node(TERM, _terms.back(), nullptr, nullptr);
+          _terms.push_back(createTerm());          
+          Node * right = new Node(TERM, _terms.back(), nullptr, nullptr);
+          Node * root = new Node(EQUALITY, nullptr, left, right);
+          _expressionTree = root;
+        }
+        else if(_currentToken == ','){
+          isCOMMA = 1;
+          Node * left = _expressionTree;
+          matchings();
+          Node * root = new Node(COMMA, nullptr, left, expressionTree());
+          _expressionTree = root;
+          
+        }
+        else if(_currentToken == ';'){
+          isCOMMA = 0;
+          Node * left = _expressionTree;
+          matchings();
+          Node * root = new Node(SEMICOLON, nullptr, left, expressionTree());
+          _expressionTree = root;
+        }
+      }
+    }
+  }
+
+  Term * find(Term * term){
+    for(int index = 0; index < _terms.size() ; index++){
+      if(_terms[index]->symbol() == term->symbol()) return _terms[index];
+      Struct * s = dynamic_cast<Struct*>(_terms[index]);
+      if(s) {
+        return findStruct(s,term);
+      }
+    }
+    return nullptr;
+  }
+
+  Term * findStruct(Struct * s, Term * term){
+    for(int i = 0; i < s->arity() ; i++){
+      if(s->args(i)->symbol() == term->symbol()) return s->args(i);
+      Struct * ss = dynamic_cast<Struct*>(s->args(i));
+      if(ss) {
+        return findStruct(ss,term);
+      }
+    }
+  }
+
+
+  Node * expressionTree(){
+    return _expressionTree;
+  }
+
 private:
   FRIEND_TEST(ParserTest, createArgs);
   FRIEND_TEST(ParserTest,ListOfTermsEmpty);
@@ -92,5 +155,7 @@ private:
   vector<Term *> _terms;
   Scanner _scanner;
   int _currentToken;
+  Node * _expressionTree;
+  int isCOMMA = 0;
 };
 #endif
